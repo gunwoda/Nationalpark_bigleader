9ab053996e82dd8b4fa29e4517d98b576a376f88
Jeongmin Ha <94777655+Hipposdata@users.noreply.github.com>
2024-04-07 23:14:28 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -137,8 +137,8 @@ def make_pointplot(selected_national_park_accident,selected_npark_boundary):
 
         # 사고 위치에 대한 CircleMarker 추가 및 툴팁 정보 설정
         for idx, row in type_accident.iterrows():
-            tooltip_text = f"유형: {row['유형']}<br>사고 일자: {row['연월일']}<br>위치: {row['위도_변환']}, {row['경도_변환']}<br>사고장소: {row['위치']}<br>고도: {row['고도']}<br>경사도: {row['경사도']}"
-            folium.CircleMarker(
+                tooltip_text = f"유형: {row['유형']}<br>사고 일자: {row['연월일']}<br>위치: {row['위도_변환']}, {row['경도_변환']}<br>사고장소: {row['위치']}<br>고도: {row['고도']:.2f}<br>경사도: {row['경사도']:.2f}"            
+                folium.CircleMarker(
                 location=(row['위도_변환'], row['경도_변환']),
                 radius=3,
                 color=accident_color,
@@ -448,7 +448,7 @@ def make_hotspot_safetyplace(selected_national_park_accident,selected_npark_boun
 
         # 사고 위치에 대한 CircleMarker 추가 및 툴팁 정보 설정
         for idx, row in type_accident.iterrows():
-            tooltip_text = f"유형: {row['유형']}<br>사고 일자: {row['연월일']}<br>위치: {row['위도_변환']}, {row['경도_변환']}<br>사고장소: {row['위치']}<br>고도: {row['고도']}<br>경사도: {row['경사도']}"
+                tooltip_text = f"유형: {row['유형']}<br>사고 일자: {row['연월일']}<br>위치: {row['위도_변환']}, {row['경도_변환']}<br>사고장소: {row['위치']}<br>고도: {row['고도']:.2f}<br>경사도: {row['경사도']:.2f}"            
             folium.CircleMarker(
                 location=(row['위도_변환'], row['경도_변환']),
                 radius=3,
@@ -690,7 +690,7 @@ def make_hotspot_heart(selected_national_park_accident,selected_npark_boundary,d
     # 심장문제 사고지점 추가
     seoul_accident_heart_layer = folium.FeatureGroup(name='심장사고지점')
     for idx, row in selected_national_park_accident.iterrows():
-        tooltip_text = f"유형: {row['유형']}<br>사고 일자: {row['연월일']}<br>위치: {row['위도_변환']}, {row['경도_변환']}<br>사고장소: {row['위치']}<br>고도: {row['고도']}<br>경사도: {row['경사도']}"
+        tooltip_text = f"유형: {row['유형']}<br>사고 일자: {row['연월일']}<br>위치: {row['위도_변환']}, {row['경도_변환']}<br>사고장소: {row['위치']}<br>고도: {row['고도']:.2f}<br>경사도: {row['경사도']:.2f}"            
         folium.CircleMarker(
             location=(row['위도_변환'], row['경도_변환']),
             radius=3,
@@ -1028,7 +1028,7 @@ def make_hotspot_fall(selected_national_park_accident,selected_npark_boundary,df
     # 추락사 사고지점 추가
     seoul_accident_fall_layer = folium.FeatureGroup(name='추락사고지점')
     for idx, row in selected_national_park_accident.iterrows():
-        tooltip_text = f"유형: {row['유형']}<br>사고 일자: {row['연월일']}<br>위치: {row['위도_변환']}, {row['경도_변환']}<br>사고장소: {row['위치']}<br>고도: {row['고도']}<br>경사도: {row['경사도']}"
+        tooltip_text = f"유형: {row['유형']}<br>사고 일자: {row['연월일']}<br>위치: {row['위도_변환']}, {row['경도_변환']}<br>사고장소: {row['위치']}<br>고도: {row['고도']:.2f}<br>경사도: {row['경사도']:.2f}"            
         folium.CircleMarker(
             location=(row['위도_변환'], row['경도_변환']),
             popup=row['사고장소'],
