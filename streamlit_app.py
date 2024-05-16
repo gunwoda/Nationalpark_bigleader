d97244f342a544c1e5daf29516a6a9af23817b7d
Hye-joon Jeon <137747849+hyejoony@users.noreply.github.com>
2024-05-16 23:28:57 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2352,6 +2352,105 @@ def heart_model(heart_gdf):
     trail_layer.add_to(m)
     smartwatch_layer.add_to(m)
     heat_layer.add_to(m)
+    template5 = """
+    {% macro html(this, kwargs) %}
+
+    <!doctype html>
+    <html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>jQuery UI Draggable - Default functionality</title>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    
+    <script>
+    $( function() {
+        $( "#maplegend2" ).draggable({
+                        start: function (event, ui) {
+                            $(this).css({
+                                right: "auto",
+                                top: "auto",
+                                bottom: "auto"
+                            });
+                        }
+                    });
+    });
+
+    </script>
+    </head>
+    <body>
+    <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-ekzFgdUyJibDJSR+Jb+wdX4Axq7rKjHcXdMI1a3iFxoZou1GEGq3w5LaD1aMhohc" crossorigin="anonymous">
+    </head>
+    
+    <div id='maplegend2' class='maplegend' 
+        style='position: absolute; z-index:9999; border:2px grey; background-color:rgba(255, 255, 255, 0.8);
+        border-radius:6px; padding: 10px; font-size:13px; left: 10px; bottom: 150px;'>
+        
+    <div class='legend-title'>스마트워치 심박수 데이터 범례</div>
+    <div class='legend-scale'>
+    <ul class='legend-labels'>
+        <li><span style='background:#E1C6A9;opacity:1;'></span><strong>전체 데이터 75% 미만 범위</strong></li>
+        <li><span style='background:#BC8344;opacity:1;'></span><strong>전체 데이터 75% 이상 범위</strong></li>
+        <li><span style='background:#664724;opacity:1;'></span><strong>기존 범위에서 벗어난 이상치</strong></li>
+
+
+    </ul>
+    </div>
+    </div>
+    
+    </body>
+    </html>
+
+    <style type='text/css'>
+    .maplegend .legend-title {
+        text-align: left;
+        margin-bottom: 5px;
+        font-weight: bold;
+        font-size: 90%;
+        }
+    .maplegend .legend-scale ul {
+        margin: 0;
+        margin-bottom: 5px;
+        padding: 0;
+        float: left;
+        list-style: none;
+        }
+    .maplegend .legend-scale ul li {
+        font-size: 80%;
+        list-style: none;
+        margin-left: 0;
+        line-height: 18px;
+        margin-bottom: 2px;
+        }
+    .maplegend ul.legend-labels li span {
+        display: block;
+        float: left;
+        height: 16px;
+        width: 30px;
+        margin-right: 5px;
+        margin-left: 0;
+        border: 1px solid #999;
+        }
+    .maplegend .legend-source {
+        font-size: 80%;
+        color: #777;
+        clear: both;
+        }
+    .maplegend a {
+        color: #777;
+        }
+    </style>
+    {% endmacro %}"""
+                
+    # Add the legend to the map
+    macro = MacroElement()
+    macro._template = Template(template5)
+    macro.add_to(m)
+
 
     # 점 찍고 점과 점 사이 거리 재기
     m.add_child(MeasureControl())
